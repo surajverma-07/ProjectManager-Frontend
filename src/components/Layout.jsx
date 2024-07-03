c5b11f804e8f40d7d9aedb9aaf7b2d50ce735eae
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2024-07-03 18:34:53 +0530

    layout changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Layout({children}) {
   return (
-    <div className='w-full max-w-screen mx-auto p-4'>
+    <div className='w-full max-w-screen mx-auto p-4 md:p-0 '>
       {children}
     </div>
   )
